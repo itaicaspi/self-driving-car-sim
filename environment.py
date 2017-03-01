99afa9b732c49bd0bf0102920f822f7c95dceee3
itaicaspi <itaicaspi@gmail.com>
2017-03-01 18:06:33 +0200

    bug fixes in environment

--- environment.py
+++ environment.py
@@ -15,30 +15,36 @@ from flask import Flask
 from io import BytesIO
 import threading
 
+#eventlet.monkey_patch(thread=True)
 sio = socketio.Server()
 app = Flask(__name__)
 _env = []
 
 class Environment:
 	def __init__(self, action_type="continuous"):
+		global _env
+		_env = self
+
 		# initialize car parameters
 		self.steering_angle = 0
 		self.throttle = 0
 		self.speed = 0
 		self.is_on_road = True
 		self.observation = []
-		
+		self.action = "None"
+		self.waiting_for_action_execution = False
+
 		# action space
-		self.steering_delta = 5
-		self.throttle_delta = 5
+		self.steering_delta = 0.01
+		self.throttle_delta = 0.1
 		self.action_type = action_type
 		if self.action_type == "continuous":
 			self.action_space = []
 		elif self.action_type == "discrete":
 			self.action_space = {
 				"None": [0, 0],
-				"Left": [-self.steering_delta, 0], 
-				"Right": [self.steering_delta, 0], 
+				"Left": [-self.steering_delta, 0],
+				"Right": [self.steering_delta, 0],
 				"Up": [0, self.throttle_delta], 
 				"Down": [0, -self.throttle_delta]
 			}
@@ -46,19 +52,30 @@ class Environment:
 		# server communication
 		self.is_connected = False
 		self.connect()
-		global _env
-		_env = self
+
 
 	def step(self, action):
+		self.action = action
+		print(_env.steering_angle, _env.throttle, _env.speed, _env.is_on_road)
+		self.waiting_for_action_execution = True
+		while self.waiting_for_action_execution:
+			pass
+		return self.steering_angle, self.throttle, self.speed, self.is_on_road
+
+	def _step(self, action):
+		if action == None:
+			return
+
+		self.action = action
 		# update control values
 		if self.action_type == "discrete":
 			action = self.action_space[action]
 
-		self.steering_angle += action[0]
-		self.throttle += action[1]
-			
+		steering_angle = self.steering_angle + action[0]
+		throttle = self.throttle + action[1]
+
 		# send to game server
-		sio.emit("steer", data={'steering_angle': self.steering_angle.__str__(), 'throttle': self.throttle.__str__()}, skip_sid=True)
+		sio.emit("steer", data={'steering_angle': steering_angle.__str__(), 'throttle': throttle.__str__()})
 
 	def reset(self):
 		# reset environment
@@ -73,10 +90,8 @@ class Environment:
 		wst = threading.Thread(target=self.serve_app, args=(sio, app))
 		wst.daemon = True
 		wst.start()
-		while self.is_connected:
+		while not self.is_connected:
 			pass
-
-#env = Environment("discrete")
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -86,11 +101,13 @@ def telemetry(sid, data):
 	_env.speed = float(data["speed"])
 	_env.is_on_road = data["is_on_road"]
 	_env.observation = data["image"]
+	_env._step(_env.action)
+	_env.waiting_for_action_execution = False
+	#_env.action = None
 
 @sio.on('connect')
 def connect(sid, environ):
 	global _env
 	print("Connect to ", sid)
 	_env.is_connected = True
-	_env.step("None")
-		
+	_env._step("None")
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-from environment import Environment
+from environment import *
 import time
-
+import random
+env = Environment("discrete")
 if __name__ == "__main__":
 	env = Environment("discrete")
 	while True:
+		#env.action = random.choice(list(env.action_space.keys()))
 		env.step("Up")
-		time.sleep(1)
+		time.sleep(0.3)
+		pass
+		#env.step("Left")
+
